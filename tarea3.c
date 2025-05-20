e1fffdb2234647586dcbcada0f71f2b367678be8
Your Name <you@example.com>
2025-05-19 21:54:25 -0400

    Prueba de actualizacion

--- tarea3.c
+++ tarea3.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include "TDAS/list.c"
+#include "TDAS/list.h"
+#include "TDAS/queue.h"
+#include "TDAS/stack.h"
+#include "TDAS/map.h"
 
 int main(){
     return 0;
